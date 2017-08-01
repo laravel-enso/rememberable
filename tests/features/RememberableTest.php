3ad01bf531925e7ed2b94af06be765be5e8f7e5f
Ionut <ionut.pirvulescu@evolution-team.ro>
2017-08-01 19:15:09 +0300

    added tests

---
+++ tests/features/RememberableTest.php
@@ -0,0 +1,67 @@
+<?php
+
+use Faker\Factory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
+use LaravelEnso\RememberableModels\app\Traits\Rememberable;
+use LaravelEnso\TestHelper\app\Classes\TestHelper;
+
+class RememberableTest extends TestHelper
+{
+    private $faker;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->faker = Factory::create();
+        $this->createRememberableModelsTable();
+    }
+
+    /** @test */
+    public function adds_model_to_cache_when_creating()
+    {
+        $rememberableModel = RememberableModel::create(['name' => $this->faker->word]);
+
+        $this->assertEquals(cache()->get('RememberableModel'.$rememberableModel->id), $rememberableModel);
+    }
+
+    /** @test */
+    public function updated_model_in_cache_when_updating()
+    {
+        $rememberableModel = RememberableModel::create(['name' => $this->faker->word]);
+
+        $rememberableModel->name = 'Updated';
+        $rememberableModel->save();
+
+        $this->assertTrue(cache()->get('RememberableModel'.$rememberableModel->id)->name === 'Updated');
+    }
+
+    /** @test */
+    public function remove_model_from_cache_when_deleting()
+    {
+        $rememberableModel = RememberableModel::create(['name' => $this->faker->word]);
+
+        $rememberableModel->delete();
+
+        $this->assertNull($rememberableModel->fresh());
+        $this->assertFalse(cache()->has('RememberableModel'.$rememberableModel->id));
+    }
+
+    private function createRememberableModelsTable()
+    {
+        Schema::create('rememberable_models', function ($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+}
+
+class RememberableModel extends Model
+{
+    use Rememberable;
+
+    protected $fillable = ['name'];
+}
+
