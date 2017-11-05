8f10d7a63c828f31a647574f5a3f7ddb68f97779
mishu <mishu@xtelecom.ro>
2017-11-05 07:55:48 +0200

    fix cacheReader

--- src/app/Traits/CacheReader.php
+++ src/app/Traits/CacheReader.php
@@ -4,22 +4,19 @@ namespace LaravelEnso\RememberableModels\app\Traits;
 
 trait CacheReader
 {
-    public function getModelFromCache($class, $id)
+    private function getModelFromCache($class, $id)
     {
+        if (!$id) {
+            return null;
+        }
+
         $model = null;
 
-        if (!cache()->has($class.$id)) {
+        if (!cache()->has($class . $id)) {
             $model = $class::find($id);
-
-            if (!$model) {
-                throw new \LogicException(
-                    __(sprintf('No model of class: %s having the id: %s found', $class, $id))
-                );
-            }
-
             $class::addOrUpdateInCache($model);
         }
 
-        return $model ?: cache()->get($class.$id);
+        return $model ?: cache()->get($class . $id);
     }
 }
