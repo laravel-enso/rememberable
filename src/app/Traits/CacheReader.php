f056d24c94e5380bfb48fac2940b7aa87272863c
mishu <mishu@xtelecom.ro>
2018-10-21 15:02:55 +0300

    updates code style

--- src/app/Traits/CacheReader.php
+++ src/app/Traits/CacheReader.php
@@ -8,14 +8,14 @@ trait CacheReader
 {
     private function getModelFromCache($class, $id)
     {
-        if (!$id) {
+        if (! $id) {
             return;
         }
 
-        if (!cache()->has($class.':'.$id)) {
+        if (! cache()->has($class.':'.$id)) {
             $model = $class::findOrFail($id);
 
-            if (!method_exists($model, 'addOrUpdateInCache')) {
+            if (! method_exists($model, 'addOrUpdateInCache')) {
                 throw new RememberableException(__(
                     'You forgot to use the `Rememberable` trait in ":class"',
                     ['class' => $class]
